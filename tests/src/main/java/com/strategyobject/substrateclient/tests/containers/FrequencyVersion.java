2a6498e15f11a5e3d5305c18163314e461695361
Peter Frank <peter.frank@unfinished.com>
2024-09-11 06:14:11 -0700

    Failing branch

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,8 +1,8 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.12.0-rc3";
-    public static final Long SPEC = 78L;
+    public static final String CURRENT_VERSION = "v1.13.0-rc4";
+    public static final Long SPEC = 103L;
 
     private FrequencyVersion() {
     }
