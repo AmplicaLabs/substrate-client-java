c73aa20956b1073ead7f9d7eb5c751010b9683b0
Peter Frank <>
2024-09-11 15:25:57 -0700

    Just cleaned up to use an Optional
    The generic type is wrong but until it's not empty it doesn't matter and
    as of now there is no plan for us to support this

--- rpc/rpc-api/src/main/java/com/strategyobject/substrateclient/rpc/api/SignedAdditionalExtra.java
+++ rpc/rpc-api/src/main/java/com/strategyobject/substrateclient/rpc/api/SignedAdditionalExtra.java
@@ -3,9 +3,12 @@ package com.strategyobject.substrateclient.rpc.api;
 import com.strategyobject.substrateclient.rpc.api.primitives.BlockHash;
 import com.strategyobject.substrateclient.scale.ScaleType;
 import com.strategyobject.substrateclient.scale.annotation.Scale;
+import com.strategyobject.substrateclient.scale.annotation.ScaleGeneric;
 import com.strategyobject.substrateclient.scale.annotation.ScaleWriter;
 import lombok.Getter;
 import lombok.NonNull;
+
+import java.util.Optional;
 
 @Getter
 @ScaleWriter
@@ -16,15 +19,21 @@ public class SignedAdditionalExtra implements AdditionalExtra {
     private final long txVersion;
     private final BlockHash genesis;
     private final BlockHash eraBlock;
-    // TODO: Convert to a real Optional and support the actual MetadataHash
-    @Scale(ScaleType.U8.class)
-    private final int metadataHash = 0;
+    @ScaleGeneric(
+        template = "Option<U8>",
+        types = {
+            @Scale(ScaleType.Option.class),
+            @Scale(ScaleType.U8.class)
+        }
+    )
+    private final Optional<Integer> metadataHash;
 
     public SignedAdditionalExtra(long specVersion, long txVersion, @NonNull BlockHash genesis, @NonNull BlockHash eraBlock) {
         this.specVersion = (int) specVersion;
         this.txVersion = (int) txVersion;
         this.genesis = genesis;
         this.eraBlock = eraBlock;
+        metadataHash = Optional.empty();
         // Set the metadata hash if we use it
         // this.metadataHash = 0;
     }
