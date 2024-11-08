4b857e4aaf269db5be025e93a32aa7fd9c8ca283
Peter Frank <peter.frank@unfinished.com>
2024-11-08 12:03:50 -0800

    Upgrading to 1.15.0

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,7 +1,7 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.13.0";
+    public static final String CURRENT_VERSION = "v1.15.0";
     public static final Long SPEC = 110L;
 
     private FrequencyVersion() {
